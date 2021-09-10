01386a03260edfb6a58a4fea2f8df0c8ec999ee0
juraph <local.juraph@gmail.com>
2021-09-11 09:50:39 +1000

    Attempt to fix

--- external/FXTUI/cmake/ftxui_export.cmake
+++
@@ -1,8 +0,0 @@
-add_library(ftxui::screen ALIAS screen)
-add_library(ftxui::dom ALIAS dom)
-add_library(ftxui::component ALIAS component)
-export(
-  TARGETS screen dom component
-  NAMESPACE ftxui::
-  FILE ${PROJECT_BINARY_DIR}/ftxui-targets.cmake
-)
--- external/FXTUI/cmake/iwyu.cmake
+++
@@ -1,7 +0,0 @@
-find_program(iwyu_path NAMES include-what-you-use iwyu)
-if(iwyu_path)
-  set_property(TARGET ${lib}
-    PROPERTY ${iwyu_path} -Xiwyu
-      --mapping_file ${CMAKE_CURRENT_SOURCE_DIR}/iwyu.impl
-  )
-endif()

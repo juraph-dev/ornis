b6c2aac9ae7b862d66555f7889807bfd4554ea0e
juraph <local.juraph@gmail.com>
2023-04-22 12:26:36 +1000

    Added some colour options!
    So many to choose from!

--- include/ornis/options.hpp
+++ include/ornis/options.hpp
@@ -72,14 +72,22 @@ private:
   unsigned rgb_;
 
   constexpr static ncselector_item home_options_[] = { { "Color scheme", "" }, { nullptr, nullptr } };
-  constexpr static ncselector_item colour_menu_[] = { { "Bluejay (Default)", "A tasteful white on blue" },
-                                                      { "Native", "Recommended for wal-nuts" },
+  constexpr static ncselector_item colour_menu_[] = { { "Bluejay", "(Default) A tasteful white on blue" },
+                                                      { "Nord", "(Dark) Vikings or something" },
+                                                      { "Monokai", "Just a great theme" },
+                                                      { "Choom", "Night City..." },
+                                                      { "Zombie", "Probably a metaphor for something" },
+                                                      { "VSCode", "Babby's first editor lmao" },
                                                       { nullptr, nullptr } };
 
+  // {Color Name, fg, bg, highlight, lowlight}
   const std::vector<color_scheme> available_colour_list = {
     { "Bluejay", { 255, 255, 255 }, { 32, 51, 70 }, { 173, 126, 77 }, { 204, 145, 109 } },
-    // FIXME Have native pull in the colours from the terminal
-    { "Native", { 32, 0, 100 }, { 1, 80, 228 }, { 200, 30, 0 }, { 0, 30, 200 } }
+    { "Nord", { 143, 188, 187 }, { 46, 52, 64 }, { 180, 142, 173 }, { 94, 129, 172 } },
+    { "Monokai", { 248, 248, 242 }, { 39, 40, 34 }, { 253, 151, 31 }, { 174, 129, 255 } },
+    { "Choom", { 158, 55, 159 }, { 55, 56, 84 }, { 232, 106, 240 }, { 123, 179, 255 } },
+    { "Zombie", { 185, 165, 137 }, { 38, 34, 29 }, { 120, 32, 32 }, { 80, 80, 60 } },
+    { "VSCode", { 255, 255, 255 }, { 39, 39, 39 }, { 30, 178, 228 }, { 55, 228, 158 } }
   };
 
   // Application configuration, currently very simple, and
